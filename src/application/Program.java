48434e7b4d2aa2e317eff48f3c73f79e28e69c06
renatocg82 <66907169+renatocg82@users.noreply.github.com>
2023-07-28 15:01:24 -0300

    Sorting employees by salary, not name

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,12 +7,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import model.entities.Employee;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		List<String> list = new ArrayList<>();
+		List<Employee> list2 = new ArrayList<>();
 		String path = "C:\\temp\\interfComp.txt";
+		String path2 = "C:\\temp\\interfComp.csv";
 
 		try(FileReader fr = new FileReader(path); BufferedReader br = new BufferedReader(fr)){
 			String name = br.readLine();
@@ -30,5 +34,32 @@ public class Program {
 		catch(IOException e) {
 			System.out.println("Error :" + e.getMessage());
 		}
+		
+		System.out.println("-----------------------------------");
+		System.out.println("-----------------------------------");
+		
+		try(FileReader fr = new FileReader(path2); BufferedReader br = new BufferedReader(fr)){
+			
+			String name = br.readLine();
+			String[] vect;
+			
+			while (name != null) {
+				vect = name.split(",");
+				
+				list2.add(new Employee(vect[0], Double.parseDouble(vect[1])));
+				name = br.readLine();
+
+			}
+			
+			Collections.sort(list2);
+			for (Employee em : list2) {
+				System.out.println(em.getName() + ", " + em.getSalary());
+			}
+
+		}
+		catch(IOException e) {
+			System.out.println("Error :" + e.getMessage());
+		}
+		
 	}
 }
